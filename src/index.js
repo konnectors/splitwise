89a7bfecc6ecda695992b10bbc068bd4e21722a9
Patrick Browne <pt.browne@gmail.com>
2018-05-24 14:00:41 +0200

    feat: send LOGIN_FAILED to stack

--- src/index.js
+++ src/index.js
@@ -12,12 +12,28 @@ const SW_EXPENSE_DOCTYPE = 'com.splitwise.expenses'
 
 let sw
 
+const translateError = e => {
+  if (e.message.includes('authentication failed')) {
+    return 'LOGIN_FAILED'
+  } else {
+    return 'UNKNOWN_ERROR'
+  }
+}
+
 // The start function is run by the BaseKonnector instance only when it got all the account
 // information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
 // the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
-  const user = await authenticate(fields.consumerKey, fields.consumerSecret)
+  let user
+  try {
+    user = await authenticate(fields.consumerKey, fields.consumerSecret)
+  } catch (e) {
+    log('error', e)
+    const msg = translateError(e)
+    log('critical', msg)
+    return
+  }
   const username = user.first_name + ' ' + user.last_name
   log('info', 'Successfully logged in for ' + username)
   const expenses = await sw.getExpenses({ limit: 100 })
